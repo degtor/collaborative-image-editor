280f0fe6fe9deb90025dfffbcf9e4db5aba55c44
VIKTOR <vikt.gustafsson@gmail.com>
2018-02-05 14:55:33 +0100

    3.8

--- js/index.js
+++ js/index.js
@@ -92,7 +92,7 @@ TogetherJS.hub.on("init", function (msg) {
 });
 
 
-TogetherJS.hub.on("reset", function (msg) {
+TogetherJS.hub.on("reseter", function (msg) {
     if (! msg.sameUrl) {
         return;
     }
@@ -106,7 +106,7 @@ $("input[type=range]").change(editImage).mousemove(editImage);
 // Reset sliders back to their original values on press of 'reset'
 $('#imageEditor').on('reset', function() {
 	if (TogetherJS.running) {
-  		TogetherJS.send({type: "reset"});
+  		TogetherJS.send({type: "reseter"});
   	}
 	
 	setTimeout(function() {
