ffcd2702625cf1c5e629678f2b7fb6da2336818c
VIKTOR <vikt.gustafsson@gmail.com>
2018-02-09 00:28:48 +0100

    final9

--- js/index.js
+++ js/index.js
@@ -350,6 +350,10 @@ $(document).on("click", "input[type=reset]", function() {
 
 $('#save_image_locally').click(function(){
 	html2canvas($('#imagesave')[0]).then(function (canvas) {
-		console.log(canvas);
+				var a = document.createElement('a');
+		        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+		        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+		        a.download = 'somefilename.jpg';
+		        a.click();
 	});
 });
